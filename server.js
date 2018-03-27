d2b66448c1e7d420412b0b7aaa223ed3ec8109c5
Ram <ramprasath25@gmail.com>
2018-03-27 23:32:10 +0530

    Added loggers

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const restify = require('restify');
 const error = require('restify-errors');
 const mongdb = require('mongodb').MongoClient;
+const logger = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const routes = require('./router/routes');
 
 const server = restify.createServer({
@@ -9,10 +12,33 @@ const server = restify.createServer({
     version: "1.0.0"
 });
 
+//Logs
+const logPath = path.join(__dirname, 'logs', 'access.log');
+if(fs.existsSync(logPath)) {
+    console.log('Logs exists');
+    server.use(logger('common', {
+        stream : fs.createWriteStream(logPath, {flags: 'a'})
+    }));
+} else {
+    fs.mkdirSync(path.dirname(logPath));
+    fs.writeFileSync(logPath, {flags : 'wx'});
+    console.log('Access Logs created');
+}
+
+server.use(logger('dev', {
+    skip: function(req, res) {
+        return res.statusCode < 400
+    }
+}));
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
 routes.applyRoutes(server, '/admin');
 server.get({path:'/', version: '1.0.0'}, (req, res, next) =>{
-    return next(new error.InternalServerError("Input missing"));
+    return next(new error.NotAuthorizedError("Input missing"));
 });
+
 server.listen(8080, () =>{
     console.log("Server is running at", server.name)
 });
